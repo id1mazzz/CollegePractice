c23f80c411007103d8d81b1ea8ba1aff3a9a4eaa
Dima Zhuk <zzzhuk.666@gmail.com>
2023-04-04 16:48:44 +0300

    uploaded the project

---
+++ OtchetS/Report.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using PdfSharp.Xps;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace OtchetS
+{
+    public static class Report
+    {
+        const int DIUPerInch = 96;
+        private static readonly Thickness defaultMargin = new Thickness(25);
+        public static void ExportVisualAsPdf(Visual visual)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                DefaultExt = ".pdf",
+                Filter = "PDF Documents (.pdf)|*.pdf"
+            };
+
+            bool? result = saveFileDialog.ShowDialog();
+
+            if (result != true) return;
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                System.IO.Packaging.Package package = System.IO.Packaging.Package.Open(memoryStream, FileMode.Create);
+                XpsDocument xpsDocument = new XpsDocument(package);
+                XpsDocumentWriter xpsDocumentWriter = XpsDocument.CreateXpsDocumentWriter(xpsDocument);
+
+                xpsDocumentWriter.Write(visual);
+                xpsDocument.Close();
+                package.Close();
+
+                var pdfXpsDoc = PdfSharp.Xps.XpsModel.XpsDocument.Open(memoryStream);
+                XpsConverter.Convert(pdfXpsDoc, saveFileDialog.FileName, 0);
+            }
+        }
+    }
+}
---
+++ OtchetS/adminWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OtchetS
+{
+    /// <summary>
+    /// Логика взаимодействия для adminWindow.xaml
+    /// </summary>
+    public partial class adminWindow : Window
+    {
+        public adminWindow()
+        {
+            InitializeComponent();
+            passportPage.Visibility = Visibility.Hidden;
+            using (var db = new Entitiess())
+            {
+                var octhetOne = db.Computers.ToList();
+                PDFcomputers.ItemsSource = octhetOne;
+
+                var v = db.Otchets.Select(ss => ss.Type).ToList();
+                PDFcomputers.ItemsSource = v.ToList();
+
+            }
+        }
+
+        private void vidOtcheta_DropDownClosed(object sender, EventArgs e)
+        {
+            using (var db = new Entitiess())
+            {
+                switch (vidOtcheta.SelectedIndex)
+                {
+                    case 0:
+                        passportPage.Visibility = Visibility.Visible; var prep = db.Computers.ToList(); PDFcomputers.ItemsSource = prep;  //Выгрузка данных из таблицы в combobox.
+                        break;
+                    case 1:
+                        passportPage.Visibility = Visibility.Collapsed;
+                        break;
+
+                }
+            }
+        }
+
+        private void bt_Print_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new Entitiess())
+            {
+                Computers sele = PDFcomputers.SelectedItem as Computers;
+
+                if (sele != null)
+                {
+                    document add = new document();
+                    add.num.Content = Convert.ToString(sele.InventNomer);
+                    add.name.Content = "Персональный компьютер " + sele.Name;
+                    add.nameTwo.Content = (sele.Name);
+                    add.kab.Content = Convert.ToString(sele.Kabinet);
+                    add.monik.Content = sele.Monitor;
+                    add.ozu.Content = sele.infoRam;
+                    add.cpu.Content = sele.infoCPU;
+                    add.mother.Content = sele.infoMBoard;
+                    add.hdd.Content = sele.infoMemory;
+                    add.vcard.Content = sele.infoVCard;
+
+                    if (add.ShowDialog() == true)
+                    {
+
+                        db.SaveChanges();
+                    }
+
+                }
+
+            }
+        }
+
+        
+    }
+}
